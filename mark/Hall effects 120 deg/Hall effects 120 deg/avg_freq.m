cb7182b9d6e6fe3d070e4e86672fb9d488225074
rnashota@student.ubc.ca <rnashota@student.ubc.ca>
2025-10-10 14:23:44 -0700

    added MTPA PI controller, need to tune

--- mark/Hall effects 120 deg/Hall effects 120 deg/avg_freq.m
+++ mark/Hall effects 120 deg/Hall effects 120 deg/avg_freq.m
@@ -1,6 +1,8 @@
 function f_Hz = avg_freq(omega_r)
 %AVG_FREQ Calculate the fundamental frequency corresponding to the running
 % average window
+
+%why over 3 window and not over 2pi
 f_Hz = max(3*max(omega_r,1e-5)/pi, 1);
 end
 
